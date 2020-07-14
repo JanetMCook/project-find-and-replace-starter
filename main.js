87562fe06f48bb566e21a71eafbecef712116b58
Janet Cook <janetmariecook@gmail.com>
2020-07-14 15:30:30 -0600

    Initial commit for assessment

--- main.js
+++ main.js
@@ -8,31 +8,34 @@ const replaceAllButton = document.querySelector(".replace-all-button")
 // The following variable holds your OUTER ARRAY of row elements.
 // Later you will need an OUTER LOOP to loop over the individual elements within
 // this array.
+
 const rowElements = document.querySelectorAll(".row")
 
-// When you call the function belwo, it will get and return an INNER ARRAY
+// When you call the function below, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
 // need to loop over the resulting cell elements. But where should this whole
 // NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
-function getCellElements (currentRowElement) {
+function getCellElements(currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
+replaceAllButton.addEventListener("click", function () {    
+        let findInputValue = findInput.value
+        let replaceInputValue = replaceInput.value
+
+        for (let outerIndex = 0; outerIndex < rowElements.length; outerIndex += 1) {
+            let row = rowElements[outerIndex];
+            let cellElements = getCellElements(row)
+
+            for (let innerIndex = 0; innerIndex < cellElements.length; innerIndex +=1) {
+                let currentCell = cellElements[innerIndex]
+                while (currentCell.innerHTML.includes(findInputValue)) {
+                    currentCell.innerHTML = currentCell.innerHTML.replace(findInputValue, replaceInputValue)
+                    
+                }
+            }
+        }
+
+})
 
 
-// YOUR CODE GOES HERE
-
-
-// One last thing: dedicate very careful attention to using variables and
-// naming them accurately.
-// And when you change the value you are assigning to a variable, don't
-// forget to consider changing the name to reflect the change you made! It
-// is very easy to get confused when you are working inside NESTED LOOPS.
-// The best of us do. And unnecessary confusion during the process of 
-// developing your code means wasted time.
-//
-// The time-cost of structuring and naming things well is FAR less than the
-// time-cost of ignoring the quality and readability of your code.
-//
-// You can, of course, remove any comments in this starter project once
-// you have read them, if you prefer.
